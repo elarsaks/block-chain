7a99d0ab19ad09990be58efa62535b7e50c4da3b
elarsaks <elarsaks@gmail.com>
2021-01-14 22:41:55 +0200

    Create Node API.

--- dev/test.js
+++ dev/test.js
@@ -1,26 +1,4 @@
 const Blockchain = require('./blockchain')
 const bitcoin = new Blockchain()
 
-const previousBlockHash = 'SS7AFT78SDTA'
-const currentBlockData = [
-  {
-    amount: 10,
-    sender: '786DSFY7DS8H',
-    recipient: '8D7S98GD7',
-  },
-  {
-    amount: 30,
-    sender: 'DSAGDFDFSG5E',
-    recipient: 'WHFTHTCHFCHBFT5RS57EY4',
-  },
-  {
-    amount: 24,
-    sender: 'RTHSDR5TU5E87',
-    recipient: 'S57S45',
-  },
-]
-const nonce = 100
-
-console.log(
-  bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce)
-)
+console.log(bitcoin)
